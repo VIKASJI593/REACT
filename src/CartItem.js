fc7db4f4d06db8a648602e96f911438c2fa81b24
VIKAS KUMAR <103217596+VIKASJI593@users.noreply.github.com>
2022-10-21 09:49:02 +0530

    Add files via upload

---
+++ src/CartItem.js
@@ -0,0 +1,70 @@
+
+import React from 'react';
+
+class CartItem extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
+    }
+   
+  }
+  increaseQuantity = () => {
+      // console.log('this', this.state);
+    this.setState({
+      qty: this.state.qty + 1
+    });
+
+  }
+  render () {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+         <img style={styles.image} alt = ""  
+         src="https://img.icons8.com/color/48/000000/rectangle.png "/> 
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://img.icons8.com/ios-glyphs/30/000000/plus--v1.png"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://img.icons8.com/ios-glyphs/30/000000/minus.png"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://img.icons8.com/fluency/48/000000/filled-trash.png"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
+}
+
+export default CartItem;
+
+
